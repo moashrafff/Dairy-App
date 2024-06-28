de5a8439060a3e486a3bc86f7aed74e5d5786ed5
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2024-06-28 23:49:33 +0300

    init commit

---
+++ app/src/main/java/com/moashraf/diaryapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.moashraf.diaryapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.moashraf.diaryapp.ui.theme.DiaryAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiaryAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+
+                }
+            }
+        }
+    }
+}
