1f96f4986ba7c5fa283e07074fa05072b7ebddfd
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2024-06-29 22:01:00 +0300

    setup mongo db

---
+++ app/src/main/java/com/moashraf/diaryapp/Constants.kt
@@ -0,0 +1,6 @@
+package com.moashraf.diaryapp
+
+object Constants {
+    const val APP_ID = "application-0-dekuiue"
+    const val CLIENT_ID = "1026206619429-9k94qvnoe4gfi2m7h0ebbk457ggij7if.apps.googleusercontent.com"
+}
