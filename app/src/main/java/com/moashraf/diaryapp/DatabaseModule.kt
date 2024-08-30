0e099ce444aa8c191d60f9f98e99618d2683eee0
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2024-08-30 17:53:34 +0300

    implement fetch images from firebase

---
+++ app/src/main/java/com/moashraf/diaryapp/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.moashraf.diaryapp
+
+import android.content.Context
+import androidx.room.Room
+import com.moashraf.diaryapp.data.database.ImageDatabase
+import com.moashraf.diaryapp.utils.Constants.IMAGES_DATABASE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): ImageDatabase {
+        return Room.databaseBuilder(
+            context = context,
+            klass = ImageDatabase::class.java,
+            name = IMAGES_DATABASE
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirstDao(database: ImageDatabase) = database.imageToUploadDao()
+}
--- app/src/main/java/com/moashraf/diaryapp/MainActivity.kt
+++ app/src/main/java/com/moashraf/diaryapp/MainActivity.kt
@@ -8,21 +8,30 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
 import com.google.firebase.initialize
+import com.moashraf.diaryapp.data.database.ImageToUploadDao
 import com.moashraf.diaryapp.navigation.Screen
 import com.moashraf.diaryapp.navigation.SetNavGraph
 import com.moashraf.diaryapp.ui.theme.DiaryAppTheme
 import com.moashraf.diaryapp.utils.Constants.APP_ID
+import com.moashraf.diaryapp.utils.retryUploadingImageToFirebase
 import dagger.hilt.android.AndroidEntryPoint
 import io.realm.kotlin.mongodb.App
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var imageToUploadDao: ImageToUploadDao
     private var keepSplashOpened = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +51,41 @@ class MainActivity : ComponentActivity() {
                     navController = navController
                 )
             }
+        }
+        cleanupCheck(
+            scope = lifecycleScope,
+            imageToUploadDao = imageToUploadDao,
+//            imageToDeleteDao = imageToDeleteDao
+        )
+    }
+    private fun cleanupCheck(
+        scope: CoroutineScope,
+        imageToUploadDao: ImageToUploadDao,
+//        imageToDeleteDao: ImageToDeleteDao
+    ) {
+        scope.launch(Dispatchers.IO) {
+            val result = imageToUploadDao.getAllImages()
+            result.forEach { imageToUpload ->
+                retryUploadingImageToFirebase(
+                    imageToUpload = imageToUpload,
+                    onSuccess = {
+                        scope.launch(Dispatchers.IO) {
+                            imageToUploadDao.cleanupImage(imageId = imageToUpload.id)
+                        }
+                    }
+                )
+            }
+//            val result2 = imageToDeleteDao.getAllImages()
+//            result2.forEach { imageToDelete ->
+//                retryDeletingImageFromFirebase(
+//                    imageToDelete = imageToDelete,
+//                    onSuccess = {
+//                        scope.launch(Dispatchers.IO) {
+//                            imageToDeleteDao.cleanupImage(imageId = imageToDelete.id)
+//                        }
+//                    }
+//                )
+//            }
         }
     }
     private fun getStartDestination():String{
